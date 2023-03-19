bcfe4bf9c2cc4edad15fd8e1e78fa15cda020a69
Bo Tian <tianbo152@gmail.com>
2023-03-19 04:56:09 +0000

    angular

--- react/jsx/src/index.js
+++ react/jsx/src/index.js
@@ -1,13 +1,26 @@
 //1) import the react and reactdom libraries
 import React from 'react';
 import  ReactDOM   from 'react-dom/client';
+
 //2) get a reference to the div with ID root
-const el = element.getElementById('root');
+const el = document.getElementById('root');
+
 //3) tell react to take control of that element
 const root = ReactDOM.createRoot(el);
+
 //4)create a component
 function App(){
-  return <h1>Hi There!</h1>;
+  let message = 'Bye there!';
+  if (Math.random() > 0.5) {
+    message = 'Hello there';
+  }
+  const date = new Date();
+  const time = date.toLocaleTimeString();
+  return <h1>{ time } </h1>;
+
+  ;
+
 }
+
 //5)show the component on the screen
 root.render(<App />)
