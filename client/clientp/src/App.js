ed19296068b10b0878be594e961115455a325dbd
Bo Tian <tianbo152@gmail.com>
2023-03-24 07:54:00 +0000

    final project

---
+++ client/clientp/src/App.js
@@ -0,0 +1,12 @@
+import  React  from "react";
+import  PostCreate1  from "./PostCreate1";
+function App() {
+  return (
+  <div className="container">
+    <h1>Create Post</h1>
+    <PostCreate1 />
+  </div>
+  );
+};
+
+export default App;
