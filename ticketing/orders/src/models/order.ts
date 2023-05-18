7ffe1b50273d6823651c59b9bb671158f39506fd
Timber <tianbo152@gmail.com>
2023-05-18 23:13:28 +0800

    order service

---
+++ ticketing/orders/src/models/order.ts
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+interface OrderAttrs {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+//我们创建这两个单独的接口的原因是，创建订单所需的属性可能与订单上实际的属性不同
+//这个服务最终将它们第一次以一组不同的属性结束，他们暂时是相同的
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc>{
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  expiresAt: {
+    type: mongoose.Schema.Types.Date
+  },
+  ticket: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Ticket'
+  }
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id
+      delete ret._id
+    }
+  }
+});
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+export { Order };
